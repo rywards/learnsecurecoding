5e441dc7e71c15d22cb142fad38091d1c91f56ff
Matt Williams <willia137@students.rowan.edu>
2021-03-08 15:29:59 -0500

    Instantiation of the Base of the Django Project

---
+++ Django_Project/Application/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['url', 'username', 'email', 'groups'] # example of insert wanted fields
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta: 
+        model = Group
+        fields = ['url','name'] # example of insert wanted fields
