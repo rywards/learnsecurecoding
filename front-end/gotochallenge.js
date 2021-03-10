9e3541296531675a0ac93353ee0f32a5c659cb60
Ryan E <rywards@ymail.com>
2021-03-10 01:16:34 -0500

    Added more styling and fixed some formatting on the pages

---
+++ front-end/gotochallenge.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Button has been clicked';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#js_button_function');
+ReactDOM.render(e(LikeButton), domContainer);
