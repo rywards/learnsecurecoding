593b0785d14745741572666881275db7e6081088
Ubuntu <ubuntu@ip-172-31-54-21.ec2.internal>
2021-03-13 03:36:56 +0000

    Updated make config file to include different

--- Django_Project/make_config.py
+++ Django_Project/make_config.py
@@ -1,4 +1,8 @@
 # Super simple script to make the .env file 
 if __name__ == '__main__':
-    file = open('.\\Django_Project\\.env', mode='x', encoding='UTF-8')
+   #Windows version
+   #file = open('.\\Django_Project\\.env', mode='x', encoding='UTF-8')
+   #Ubuntu Version
+    file = open('.env', mode = 'x', encoding='UTF-8')
     file.close()
+
