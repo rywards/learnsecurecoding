e7ae05bc5cacb850139aba49b7df238b99d49adb
JL102 <jordannamez@gmail.com>
2021-05-10 16:01:05 -0400

    fixed thing that wasn't commented out

--- Django_Project/Application/cwe_checker.py
+++ Django_Project/Application/cwe_checker.py
@@ -111,7 +111,7 @@ def JoinCompileAndRun(fileName, userCode):
 			fail_reasons.append(reason)
 		
 	# and delete source+executable when done
-	#delete(srcCodePath)
+	delete(srcCodePath)
 	delete(executablePath)
 	delete(executablePathWin)
 	
