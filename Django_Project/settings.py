9cfa376003a95fbe7f9b580cd9ee64dc864229e5
MathewTWilliams <williams.mathew.t@gmail.com>
2021-05-11 21:05:35 -0400

    Fixed settings.py so that it includes the correct database user and password.

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config("DATABASE_NAME"),
-        'USER': "ProggyPanda",
-        'PASSWORD' :  "ProggyPanda",
+        'USER': config("DATABASE_USER"),
+        'PASSWORD' : config("DATABASE_PASSWORD"),
         'HOST' : config("DATABASE_HOST"),
         'PORT' : config("DATABASE_PORT")
 
